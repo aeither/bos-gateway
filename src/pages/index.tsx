8ce6e9ea41b9141fd6ad3b5466e17fd6ab440367
aeither <36173828+aeither@users.noreply.github.com>
2023-10-11 18:42:59 +0300

    add component with props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-import { useBosComponents } from '@/hooks/useBosComponents';
-import { useDefaultLayout } from '@/hooks/useLayout';
-import type { NextPageWithLayout } from '@/utils/types';
-import { ComponentWrapperPage } from '@/components/ComponentWrapperPage';
+import { useBosComponents } from "@/hooks/useBosComponents";
+import { useDefaultLayout } from "@/hooks/useLayout";
+import type { NextPageWithLayout } from "@/utils/types";
+import { ComponentWrapperPage } from "@/components/ComponentWrapperPage";
 
 const HomePage: NextPageWithLayout = () => {
   const components = useBosComponents();
-  return <ComponentWrapperPage src={components.home} />;
+
+  return (
+    <>
+      <ComponentWrapperPage src={components.home} />
+      <ComponentWrapperPage
+        src={
+          "f54a9bc1772d359e09686dc19b32d291234bdbaa685cdd87ff1ef68d3c0dc74a/widget/tset1"
+        }
+      />
+      <ComponentWrapperPage
+        src={
+          "f54a9bc1772d359e09686dc19b32d291234bdbaa685cdd87ff1ef68d3c0dc74a/widget/kitchensink"
+        }
+        componentProps={{ name: "jack" }}
+      />
+    </>
+  );
 };
 
 HomePage.getLayout = useDefaultLayout;
